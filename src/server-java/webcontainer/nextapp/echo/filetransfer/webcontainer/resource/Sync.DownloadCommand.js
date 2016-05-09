17e552f797fa5e6a180d3bc9442b3cc5667270f4
Lloyd Colling <lloyd.colling@oracle.com>
2016-05-09 13:40:19 +0100

    Detect a browserUnload listener on the window, and unassign it temporarily so that downloading a file does not prompt the user as if they were leaving the page.

--- src/server-java/webcontainer/nextapp/echo/filetransfer/webcontainer/resource/Sync.DownloadCommand.js
+++ src/server-java/webcontainer/nextapp/echo/filetransfer/webcontainer/resource/Sync.DownloadCommand.js
@@ -1,14 +1,35 @@
 /**
  * Command exeecution peer: Download
  */
+Echo.RemoteClient.CommandExec.DownloadCallback = Core.extend(Core.Web.Scheduler.Runnable, {
+    _beforeUnloadHandler: null,
+    timeInterval: 300,
+    
+    $construct: function(beforeUnloadHandler) {
+        this._beforeUnloadHandler = beforeUnloadHandler;
+    },
+    
+    run: function () {
+        window.onbeforeunload = this._beforeUnloadHandler;
+    }
+});
+
 Echo.RemoteClient.CommandExec.Download = {
 
     execute: function(client, commandData) {
         if (!commandData.uri) {
             throw new Error("uri not specified in DownloadCommand.");
         }
+        var beforeUnloadHandler = null;
+        if (window.onbeforeunload !== undefined && window.onbeforeunload !== null) {
+            beforeUnloadHandler = new Echo.RemoteClient.CommandExec.DownloadCallback(window.onbeforeunload);
+            window.onbeforeunload = null;
+        }
         top.location = commandData.uri;
-    }    
+        if (beforeUnloadHandler !== null) {
+            Core.Web.Scheduler.add(beforeUnloadHandler);
+        }
+    }
 };
 
 Echo.RemoteClient.CommandExecProcessor.registerPeer("nextapp.echo.filetransfer.app.DownloadCommand", 
